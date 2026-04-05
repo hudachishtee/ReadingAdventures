faf49eba6c4d5582ce9890417b47d37550321c84
huda <hudachishtee@gmail.com>
2026-04-05 09:57:53 +0300

    Initial Commit

---
+++ ReadingAdventures/ReadingAdventuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingAdventuresApp.swift
+//  ReadingAdventures
+//
+//  Created by Huda Chishtee on 05/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ReadingAdventuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
